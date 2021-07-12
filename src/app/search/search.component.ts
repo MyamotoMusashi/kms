e6d02847d029a7179467f1b073e75805aed7c565
unknown <knifed313@gmail.com>
2021-07-12 15:45:47 +0300

    added timer to the search bar at the top that shows the remaining hours of your shift

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UrlsService } from '../urls.service';
 import { Url } from '../models/url.model';
 import { Router } from '@angular/router';
+import { interval, Observable, Subscription } from 'rxjs';
+import { MinLengthValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -11,6 +13,10 @@ import { Router } from '@angular/router';
 export class SearchComponent implements OnInit {
   data = [] as Url[];
   keyword = 'url';
+  remainingTime
+  remainingTimeSubscription = new Subscription();
+  
+
 
   constructor(private urlsService: UrlsService, private router: Router) {}
 
@@ -19,6 +25,13 @@ export class SearchComponent implements OnInit {
       this.data = urls;
       console.log(this.data.length);
     });
+    this.remainingTimeSubscription = interval(1000)
+           .subscribe(x => {
+            let currentTime = new Date(),
+            midnight = new Date()
+            midnight.setHours(26 - currentTime.getHours(), 0 - currentTime.getMinutes(), 0 - currentTime.getSeconds());
+            this.remainingTime = `${midnight.getHours()}:${midnight.getMinutes()}:${midnight.getSeconds()} Hours`;
+            });
   }
 
   Sync(): void {
